e698d87db0977466f2827f1887a9fe8b06d68b8d
tipaye <40826923+tipaye@users.noreply.github.com>
2025-09-05 22:14:21 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,11 @@
+input.onButtonPressed(Button.A, function () {
+    if (num >= 9) {
+        num = 0
+    } else {
+        num += 1
+    }
+    basic.showNumber(num)
+})
 input.onSound(DetectedSound.Loud, function () {
     clapped = !(clapped)
     if (clapped) {
@@ -13,5 +21,7 @@ input.onSound(DetectedSound.Loud, function () {
     }
 })
 let clapped = false
+let num = 0
 basic.clearScreen()
 input.setSoundThreshold(SoundThreshold.Loud, 220)
+num = 0
